9d19c8f4850cd982a4694c9299c5f970fca7a60d
Mark_dev <39365134+andreeaRente@users.noreply.github.com>
2019-02-03 00:01:10 +0200

    Added Unit Tests

---
+++ ELORating/Unit Tests Done/UnitTestProject1/UnitTestProject2/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ELORating;
+using System.Collections;
+
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double newRanking;
+            double currentRanking = 1234;
+            double provisionalRanking;
+            string pathTokey = @"C:\Users\andreea.rentea\Documents\Visual Studio 2015\Projects\ELORating\key.txt";
+
+            ELOClient client = new ELOClient();
+            string tierout = client.readSeedData("TossThoseDigits2");//given a summoner name, read from seed data
+
+            DataPlayer datagame = new DataPlayer(1149, 1150, 1499, 1500, 1849, 1850, 2199, 2200, 2499, 2500, 2999);//limits for ranking
+
+            if (tierout == null || tierout.ToLower() == "unranked")
+            {
+                provisionalRanking = client.calculateProvisionalElo("TossThoseDigits2", 0, datagame);
+                currentRanking = provisionalRanking;
+            }
+
+            //create teams
+            Hashtable htAllies = new Hashtable();
+
+            htAllies.Add("TossThoseDigits2", currentRanking);
+            htAllies.Add("141Masters", 1290);
+            htAllies.Add("Pheejhmoo", 1232);
+            htAllies.Add("DUNKNGOHAMZIES", 1289);
+            htAllies.Add("YABO2", 1700);
+
+
+            Hashtable htOpponents = new Hashtable();
+            htOpponents.Add("TheMuffinMan97", 1567);
+            htOpponents.Add("GXuanMing", 1800);
+            htOpponents.Add("vampiresland", 1100);
+            htOpponents.Add("XxlivewirexX", 1311);
+            htOpponents.Add("Molegg", 1200);
+
+            //calculate elo after match
+            newRanking = client.calculateElo("141Masters", currentRanking, "win", htAllies, htOpponents);
+
+
+            ConstantAdjustments consta = new ConstantAdjustments();
+            string adjustments = consta.AdjustMethod("TossThoseDigits2", "platinum", 1450, pathTokey); // adjust upper and lower limits; output: the name of the 
+        }
+    }
+}
