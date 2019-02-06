b2c642307bd97c0a65612e0201764c14ce19f515
AlexandruRusu23 <alexandru.rusu707@gmail.com>
2019-02-06 14:03:01 +0200

    get player most used champs

--- Fetchy/LoLFetcher.py
+++ Fetchy/LoLFetcher.py
@@ -139,6 +139,22 @@ class LoLFetcher(DataFetcher.DataFetcher):
         except Exception:
             print('Player {player_name} not found on url'.format(player_name = player_name))
             return [-1 for i in range(3)]
+    
+    def get_player_muc(self, root_url = str(), region = str(), player_name = str()):
+        '''
+        This method returns player's most used champs data searching by name
+        '''
+        try:
+            full_url = root_url + region + '/' + player_name
+            raw_html = self.simple_get(full_url)
+            soup = DataFetcher.BeautifulSoup(raw_html, 'html.parser')
+            player_kda = soup.find("div",{"class":"s-block2"})
+            muchamps_html = player_kda.find_all('span', {'class':'champid'})
+            muchamps_list = [iter.text for iter in muchamps_html]
+            #return top 5 most used champs
+            return ','.join(muchamps_list[:5])
+        except Exception:
+            return 'No record.'
 
     def store_leaderboard_data(self, root_url = str(), region = str()):
         '''
@@ -177,7 +193,8 @@ class LoLFetcher(DataFetcher.DataFetcher):
                 player_data_list[LoLFetcher.DATABASE_INDEX_LOSSES] = current_list[LoLFetcher.FETCHER_INDEX_LOSSES]
                 player_data_list[LoLFetcher.DATABASE_INDEX_DIVISION] = current_list[LoLFetcher.FETCHER_INDEX_DIVISION]
                 player_data_list[LoLFetcher.DATABASE_INDEX_POINTS] = current_list[LoLFetcher.FETCHER_INDEX_POINTS]
-                #player_data_list[LoLFetcher.DATABASE_INDEX_MOST_USED_CHAMPS] = current_list[LoLFetcher.FETCHER_INDEX_MOST_USED_CHAMPS]
+                player_muc = self.get_player_muc(LoLFetcher.PLAYER_ROOT_URL, LoLFetcher.NA, player_data_list[LoLFetcher.DATABASE_INDEX_NAME])
+                player_data_list[LoLFetcher.DATABASE_INDEX_MOST_USED_CHAMPS] = player_muc
                 player_kda = self.get_player_kda(LoLFetcher.PLAYER_ROOT_URL, LoLFetcher.NA, player_data_list[LoLFetcher.DATABASE_INDEX_NAME])
                 player_data_list[LoLFetcher.DATABASE_INDEX_KILLS] = player_kda[0]
                 player_data_list[LoLFetcher.DATABASE_INDEX_DEATHS] = player_kda[1]
